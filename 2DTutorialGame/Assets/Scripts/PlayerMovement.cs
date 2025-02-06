639b339b3341fc432059ad0c5a597e943803bee5
Joni Lampela <93704834+LastJonicorn@users.noreply.github.com>
2025-02-06 09:57:58 +0200

    Crouching works + Something's still off with jumping

--- 2DTutorialGame/Assets/Scripts/PlayerMovement.cs
+++ 2DTutorialGame/Assets/Scripts/PlayerMovement.cs
@@ -35,6 +35,11 @@ public class PlayerMovement : MonoBehaviour
         animator.SetBool("IsJumping", false);
     }
 
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
+    }
+
     private void FixedUpdate()
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
