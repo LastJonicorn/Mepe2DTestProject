bcb3360f5db91a2c6aab1c6e2bab516b9dd87393
Joni Lampela <93704834+LastJonicorn@users.noreply.github.com>
2025-02-11 14:58:41 +0200

    Working combat system

--- 2DTutorialGame/Assets/Enemy.cs
+++ 2DTutorialGame/Assets/Enemy.cs
@@ -2,13 +2,52 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Vihollisen elämä")]
     public Animator animator;
     public int maxHealth = 100;
     public int currentHealth;
 
+    [Header("Vihollinen lyö")]
+    public Transform attackPoint;
+    public float attackRange = 0.1f;
+    public LayerMask playerLayer;
+    public Transform playerTransform;
+    public int attackDamage = 20;
+    public float attackCooldown = 2f;
+    private float lastAttackTime = -Mathf.Infinity;
+
     void Start()
     {
         currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        if (Time.time >= lastAttackTime + attackCooldown && playerTransform != null)
+        {
+            TryAttack();
+        }
+    }
+
+    void TryAttack()
+    {
+        Collider2D hitPlayer = Physics2D.OverlapCircle(attackPoint.position, attackRange, playerLayer);
+
+        if (hitPlayer != null)
+        {
+            Attack(hitPlayer);
+        }
+    }
+
+    void Attack(Collider2D player)
+    {
+        lastAttackTime = Time.time;
+
+        PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+        if (playerHealth != null)
+        {
+            playerHealth.TakeDamage(attackDamage);
+        }
     }
 
     public void TakeDamage(int damage)
@@ -31,5 +70,8 @@ public class Enemy : MonoBehaviour
         //Destroy(gameObject);
         this.enabled = false;
     }
-
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
 }
---
+++ 2DTutorialGame/Assets/PlayerHealth.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth = 100;
+    public float currentHealth;
+    public Image healthBar;
+    public Animator animator;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        healthBar.fillAmount = Mathf.Clamp(currentHealth / maxHealth, 0, 1);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        animator.SetTrigger("TakeDamage");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        var currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+
+    }
+}
